a4206375158fcbd40e3840732db66f82059068ac
josemunozmunoz@iessoterohernandez.es <josemunozmunoz@iessoterohernandez.es>
2023-04-20 19:16:15 +0200

    se

--- src/main/java/com/example/demo/Demo1Application.java
+++ src/main/java/com/example/demo/Demo1Application.java
@@ -2,11 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.system.SystemProperties;
 
 @SpringBootApplication
 public class Demo1Application {
 
 	public static void main(String[] args) {
+		System.setProperty("java.runtime.version", "1.17");
 		SpringApplication.run(Demo1Application.class, args);
 	}
 
